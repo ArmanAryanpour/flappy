632ab85eaad35cd1fcc4f834f43a3a71e0c81946
ArmanAryanpour <ArmanAryanpour@users.noreply.github.com>
2024-03-25 09:53:39 +0000

    deploy: 0cf999975f1430164b5025c4ca2f9a076ee7d3ef

---
+++ _next/static/QHev9oJu9VjLqbr01iVfc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-fbcfe2172188e46f.js","static/chunks/660-022dc1cd45fd4c07.js","static/chunks/pages/index-3c42cc5bfa2847ad.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
